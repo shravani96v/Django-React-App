2ff8017dc3c9064991c0da1c0125bad6ffd16497
shravani96v <sv1059@wildcats.unh.edu>
2020-11-08 02:35:31 -0500

    changed imports as parent model names are changed

--- transfer/models/model_transferevaluation.py
+++ transfer/models/model_transferevaluation.py
@@ -1,14 +1,14 @@
 
 from django.db import models
 from .model_transfer_course import TransferCourse
-from .model_major_requirement import Major_requirement
+from .model_major_requirement import MajorRequirement
 from .model_approver import Approver
 
 
 class Transferevaluation(models.Model):
     transfer_eval_id = models.AutoField(primary_key=True)
     transfer_course_id = models.ForeignKey(TransferCourse, on_delete=models.CASCADE)
-    major_req_id = models.ForeignKey(Major_requirement, on_delete=models.CASCADE)
+    major_req_id = models.ForeignKey(MajorRequirement, on_delete=models.CASCADE)
     sem_year_taken = models.CharField(max_length=8, blank=True, null=True)
     expiration_date = models.DateField(blank=True, null=True)  # It must be in YYYY-MM-DD format
     approved_status = models.CharField(max_length=1, blank=True, null=True)
